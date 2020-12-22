784d958bfc2f10823b93533cf202d78ed3da3f51
吴燕新 <635831101@qq.com>
2020-12-22 18:43:22 +0800

    ListBox 添加新的样式 PopularListBox

--- Rubyer/ListBoxHelper.cs
+++ Rubyer/ListBoxHelper.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 
 namespace Rubyer
 {
@@ -34,6 +37,59 @@ namespace Rubyer
         public static void SetFocusedForegroundBrush(DependencyObject obj, SolidColorBrush value)
         {
             obj.SetValue(FocusedForegroundBrushProperty, value);
+        }
+
+        // 选中动画
+        public static readonly DependencyProperty IsAnimationProperty =
+            DependencyProperty.RegisterAttached("IsAnimation", typeof(bool), typeof(ListBoxHelper), new PropertyMetadata(false, OnIsAnimationChanged));
+
+        private static void OnIsAnimationChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ListBox listBox)
+            {
+                listBox.Loaded += (sender, arg) =>
+                {
+                    if (listBox.Template.FindName("currentRectangle", listBox) is Rectangle rectangle)
+                    {
+                        if (GetIsAnimation(listBox))
+                        {
+                            listBox.SelectionChanged += (a, b) =>
+                            {
+                                int index = listBox.SelectedIndex;
+                                ListBoxItem item = listBox.ItemContainerGenerator.ContainerFromItem(listBox.Items[index]) as ListBoxItem;
+                                rectangle.Height = item.ActualHeight;
+
+                                DoubleAnimation topAnimation = new DoubleAnimation
+                                {
+                                    To = item.ActualHeight * index,
+                                    Duration = TimeSpan.FromMilliseconds(500),
+                                    EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+                                };
+
+                                rectangle.BeginAnimation(Canvas.TopProperty, topAnimation);
+                            };
+
+                            if (listBox.SelectedIndex >= 0)
+                            {
+                                int index = listBox.SelectedIndex;
+                                ListBoxItem item = listBox.ItemContainerGenerator.ContainerFromItem(listBox.Items[index]) as ListBoxItem;
+                                rectangle.Height = item.ActualHeight;
+                                rectangle.SetValue(Canvas.TopProperty, item.ActualHeight * index);
+                            }
+                        }
+                    }
+                };
+            }
+        }
+
+        public static bool GetIsAnimation(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsAnimationProperty);
+        }
+
+        public static void SetIsAnimation(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsAnimationProperty, value);
         }
     }
 }
