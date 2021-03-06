bc3d071fcb4c877a31c66ac328f3f163008376cc
吴燕新 <635831101@qq.com>
2021-03-06 14:49:06 +0800

    修复不可设置 DataGrid 行背景色的 bug ，增加可设置表头高度和行高度

--- Rubyer/DataGridHelper.cs
+++ Rubyer/DataGridHelper.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace Rubyer
@@ -36,6 +39,49 @@ namespace Rubyer
         public static Brush GetHeadForeground(DependencyObject element)
         {
             return (Brush)element.GetValue(HeadForegroundProperty);
+        }
+
+
+        // 当然行背景色
+        public static readonly DependencyProperty FocusRowBackgroundProperty =
+            DependencyProperty.RegisterAttached("FocusRowBackground", typeof(Brush), typeof(DataGridHelper));
+
+        public static void SetFocusRowBackground(DependencyObject element, Brush value)
+        {
+            element.SetValue(FocusRowBackgroundProperty, value);
+        }
+
+        public static Brush GetFocusRowBackground(DependencyObject element)
+        {
+            return (Brush)element.GetValue(FocusRowBackgroundProperty);
+        }
+
+        // 标题栏行高
+        public static readonly DependencyProperty HeadRowHeightProperty =
+            DependencyProperty.RegisterAttached("HeadRowHeight", typeof(double), typeof(DataGridHelper));
+
+        public static void SetHeadRowHeight(DependencyObject element, double value)
+        {
+            element.SetValue(HeadRowHeightProperty, value);
+        }
+
+        public static double GetHeadRowHeight(DependencyObject element)
+        {
+            return (double)element.GetValue(HeadRowHeightProperty);
+        }
+
+        // 单元格行高
+        public static readonly DependencyProperty CellRowHeightProperty =
+            DependencyProperty.RegisterAttached("CellRowHeight", typeof(double), typeof(DataGridHelper));
+
+        public static void SetCellRowHeight(DependencyObject element, double value)
+        {
+            element.SetValue(CellRowHeightProperty, value);
+        }
+
+        public static double GetCellRowHeight(DependencyObject element)
+        {
+            return (double)element.GetValue(CellRowHeightProperty);
         }
     }
 }
