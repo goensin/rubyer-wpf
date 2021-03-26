e223f72c07e683dc438af43e62b56ca8f5c32a1d
吴燕新 <635831101@qq.com>
2021-03-26 18:14:46 +0800

    增加转换动画 Transition 控件

---
+++ Rubyer/Transition.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rubyer
+{
+    public class Transition : ContentControl
+    {
+        static Transition()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Transition), new FrameworkPropertyMetadata(typeof(Transition)));
+        }
+
+        #region 依赖属性
+        public static readonly DependencyProperty IsShowProperty =
+            DependencyProperty.Register("IsShow", typeof(bool), typeof(Transition), new PropertyMetadata(default(bool)));
+
+        public bool IsShow
+        {
+            get { return (bool)GetValue(IsShowProperty); }
+            set { SetValue(IsShowProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty TypeProperty =
+            DependencyProperty.Register("Type", typeof(TransitionType), typeof(Transition), new PropertyMetadata(default(TransitionType)));
+
+        public TransitionType Type
+        {
+            get { return (TransitionType)GetValue(TypeProperty); }
+            set { SetValue(TypeProperty, value); }
+        }
+
+        #endregion
+    }
+
+    public enum TransitionType
+    {
+        Fade = 0,
+        FadeLeft,
+        FadeRight,
+        FadeUp,
+        FadeDown,
+        Zoom,
+        ZoomX,
+        ZoomY,
+        ZoomLeft,
+        ZoomRight,
+        ZoomUp,
+        ZoomDown
+    }
+}
--- RubyerDemo/ViewModels/MainViewModel.cs
+++ RubyerDemo/ViewModels/MainViewModel.cs
@@ -39,7 +39,8 @@ namespace RubyerDemo.ViewModels
                 new MenuItem{ Name = "页码条-PageBar",Content=new Views.PageBar{ DataContext = new PageBarViewModel()} },
                 new MenuItem{ Name = "消息提示-MessageInfo",Content=new MessageInfo{} },
                 new MenuItem{ Name = "消息框-MessageBoxView",Content = new MessageBoxView{ DataContext= new MessageBoxViewModel()} },
-                new MenuItem{ Name = "对话框-DialogView",Content = new DialogView{ DataContext= new DialogViewModel()} }
+                new MenuItem{ Name = "对话框-DialogView",Content = new DialogView{ DataContext= new DialogViewModel()} },
+                new MenuItem{ Name = "转换动画-TransitionView",Content = new TransitionView() }
             };
 
             CurrentMenuItem = MenuItems[0];
