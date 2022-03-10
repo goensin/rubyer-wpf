6f586b49e46420095659ec1bdda56116f6f2b6a0
ensin <635831101@qq.com>
2022-03-10 10:38:29 +0800

    优化 ToggleButton 样式

---
+++ Rubyer/Converters/BooleanToInverseConverter.cs
@@ -0,0 +1,13 @@
+﻿namespace Rubyer.Converters
+{
+    /// <summary>
+    /// bool - 取反
+    /// </summary>
+    public class BooleanToInverseConverter : BooleanConverter<bool>
+    {
+        public BooleanToInverseConverter() 
+            : base(false, true)
+        {
+        }
+    }
+}
