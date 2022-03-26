94283722ed477109035778fe68f57423c93ce22f
吴燕新 <635831101@qq.com>
2022-03-26 21:29:23 +0800

    优化 GroupBox 样式

---
+++ Rubyer/Helpers/GroupBoxHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Rubyer
+{
+    public class GroupBoxHelper
+    {
+        // HeaderBackground
+        public static readonly DependencyProperty HeaderBackgroundProperty =
+            DependencyProperty.RegisterAttached("HeaderBackground", typeof(SolidColorBrush), typeof(GroupBoxHelper), new PropertyMetadata(new SolidColorBrush(Colors.Black)));
+
+        public static SolidColorBrush GetHeaderBackground(DependencyObject obj)
+        {
+            return (SolidColorBrush)obj.GetValue(HeaderBackgroundProperty);
+        }
+
+        public static void SetHeaderBackground(DependencyObject obj, SolidColorBrush value)
+        {
+            obj.SetValue(HeaderBackgroundProperty, value);
+        }
+    }
+}
