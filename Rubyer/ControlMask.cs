6be0aa081a8cb7f95a4d82c97104352f8a4b2a08
ensin <635831101@qq.com>
2022-04-12 16:05:18 +0800

    ControlMask 增加透明度可设置

--- Rubyer/ControlMask.cs
+++ Rubyer/ControlMask.cs
@@ -14,6 +14,16 @@ namespace Rubyer
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ControlMask), new FrameworkPropertyMetadata(typeof(ControlMask)));
         }
 
+        // 是否激活
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register(
+            "IsActive", typeof(bool), typeof(ControlMask), new PropertyMetadata(default(bool)));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
         // 圆角半径
         public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
             "CornerRadius", typeof(CornerRadius), typeof(ControlMask), new PropertyMetadata(default(CornerRadius)));
@@ -22,6 +32,16 @@ namespace Rubyer
         {
             get { return (CornerRadius)GetValue(CornerRadiusProperty); }
             set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // 遮罩透明度
+        public static readonly DependencyProperty MaskOpacityProperty = DependencyProperty.Register(
+            "MaskOpacity", typeof(double), typeof(ControlMask), new PropertyMetadata(default(double)));
+
+        public double MaskOpacity
+        {
+            get { return (double)GetValue(MaskOpacityProperty); }
+            set { SetValue(MaskOpacityProperty, value); }
         }
     }
 }
