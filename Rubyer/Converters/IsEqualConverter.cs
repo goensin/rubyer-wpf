483fe6eb89217261b10db4e8cd6317c8398c29f0
ensin <635831101@qq.com>
2022-04-12 13:55:39 +0800

    删除无用资源

--- Rubyer/Converters/IsEqualConverter.cs
+++ Rubyer/Converters/IsEqualConverter.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Data;
 
 namespace Rubyer.Converters
@@ -13,7 +8,7 @@ namespace Rubyer.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return values[0].ToString() == values[1].ToString();
+            return values[0].Equals(values[1]);
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
