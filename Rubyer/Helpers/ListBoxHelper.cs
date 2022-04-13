156fb9d8a471d7f5af348763bf4f8507bd061343
ensin <635831101@qq.com>
2022-04-13 12:40:18 +0800

    优化 ListBox 样式

--- Rubyer/Helpers/ListBoxHelper.cs
+++ Rubyer/Helpers/ListBoxHelper.cs
@@ -11,34 +11,6 @@ namespace Rubyer
 {
     public static class ListBoxHelper
     {
-        // 聚焦时颜色
-        public static readonly DependencyProperty FocusedBrushProperty =
-            DependencyProperty.RegisterAttached("FocusedBrush", typeof(SolidColorBrush), typeof(ListBoxHelper), new PropertyMetadata(new SolidColorBrush(Colors.Black)));
-
-        public static SolidColorBrush GetFocusedBrush(DependencyObject obj)
-        {
-            return (SolidColorBrush)obj.GetValue(FocusedBrushProperty);
-        }
-
-        public static void SetFocusedBrush(DependencyObject obj, SolidColorBrush value)
-        {
-            obj.SetValue(FocusedBrushProperty, value);
-        }
-
-        // 聚焦前景色时颜色
-        public static readonly DependencyProperty FocusedForegroundBrushProperty =
-            DependencyProperty.RegisterAttached("FocusedForegroundBrush", typeof(SolidColorBrush), typeof(ListBoxHelper), new PropertyMetadata(new SolidColorBrush(Colors.Black)));
-
-        public static SolidColorBrush GetFocusedForegroundBrush(DependencyObject obj)
-        {
-            return (SolidColorBrush)obj.GetValue(FocusedForegroundBrushProperty);
-        }
-
-        public static void SetFocusedForegroundBrush(DependencyObject obj, SolidColorBrush value)
-        {
-            obj.SetValue(FocusedForegroundBrushProperty, value);
-        }
-
         // 选中动画
         public static readonly DependencyProperty IsAnimationProperty =
             DependencyProperty.RegisterAttached("IsAnimation", typeof(bool), typeof(ListBoxHelper), new PropertyMetadata(false, OnIsAnimationChanged));
