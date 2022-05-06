a5f663625095b7e7c2aa744b1b8e260cd112b8db
ensin <635831101@qq.com>
2022-05-06 18:17:51 +0800

    优化 DataGrid 样式

--- Rubyer/Helpers/DataGridHelper.cs
+++ Rubyer/Helpers/DataGridHelper.cs
@@ -13,63 +13,6 @@ namespace Rubyer
 {
     public static class DataGridHelper
     {
-        // 表头背景色
-        public static readonly DependencyProperty HeadBackgroundProperty =
-            DependencyProperty.RegisterAttached("HeadBackground", typeof(Brush), typeof(DataGridHelper));
-
-        public static void SetHeadBackground(DependencyObject element, Brush value)
-        {
-            element.SetValue(HeadBackgroundProperty, value);
-        }
-
-        public static Brush GetHeadBackground(DependencyObject element)
-        {
-            return (Brush)element.GetValue(HeadBackgroundProperty);
-        }
-
-        // 表头前景色
-        public static readonly DependencyProperty HeadForegroundProperty =
-            DependencyProperty.RegisterAttached("HeadForeground", typeof(Brush), typeof(DataGridHelper));
-
-        public static void SetHeadForeground(DependencyObject element, Brush value)
-        {
-            element.SetValue(HeadForegroundProperty, value);
-        }
-
-        public static Brush GetHeadForeground(DependencyObject element)
-        {
-            return (Brush)element.GetValue(HeadForegroundProperty);
-        }
-
-
-        // 当然行背景色
-        public static readonly DependencyProperty FocusRowBackgroundProperty =
-            DependencyProperty.RegisterAttached("FocusRowBackground", typeof(Brush), typeof(DataGridHelper));
-
-        public static void SetFocusRowBackground(DependencyObject element, Brush value)
-        {
-            element.SetValue(FocusRowBackgroundProperty, value);
-        }
-
-        public static Brush GetFocusRowBackground(DependencyObject element)
-        {
-            return (Brush)element.GetValue(FocusRowBackgroundProperty);
-        }
-
-        // 标题栏行高
-        public static readonly DependencyProperty HeadRowHeightProperty =
-            DependencyProperty.RegisterAttached("HeadRowHeight", typeof(double), typeof(DataGridHelper));
-
-        public static void SetHeadRowHeight(DependencyObject element, double value)
-        {
-            element.SetValue(HeadRowHeightProperty, value);
-        }
-
-        public static double GetHeadRowHeight(DependencyObject element)
-        {
-            return (double)element.GetValue(HeadRowHeightProperty);
-        }
-
         // 单元格行高
         public static readonly DependencyProperty CellRowHeightProperty =
             DependencyProperty.RegisterAttached("CellRowHeight", typeof(double), typeof(DataGridHelper));
@@ -82,20 +25,6 @@ namespace Rubyer
         public static double GetCellRowHeight(DependencyObject element)
         {
             return (double)element.GetValue(CellRowHeightProperty);
-        }
-
-        // 标题栏水平对齐方式
-        public static readonly DependencyProperty HorizontalHeadAlignmentProperty =
-            DependencyProperty.RegisterAttached("HorizontalHeadAlignment", typeof(HorizontalAlignment), typeof(DataGridHelper));
-
-        public static void SetHorizontalHeadAlignment(DependencyObject element, HorizontalAlignment value)
-        {
-            element.SetValue(HorizontalHeadAlignmentProperty, value);
-        }
-
-        public static HorizontalAlignment GetHorizontalHeadAlignment(DependencyObject element)
-        {
-            return (HorizontalAlignment)element.GetValue(HorizontalHeadAlignmentProperty);
         }
     }
 }
