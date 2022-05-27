233f7f1aae4ab408225fe0baf66b31df0288fab6
wuyanxin <635831101@qq.com>
2022-05-27 18:34:38 +0800

    增加 pack.ps1

--- Rubyer/Dialog.cs
+++ Rubyer/Dialog.cs
@@ -39,19 +39,16 @@ namespace Rubyer
         /// <param name="isShowCloseButton">是否显示默认关闭按钮</param>
         public static void Show(string identifier, object content, string title, Action<DialogBox> openHandler, Action<DialogBox, object> closeHandle, bool isShowCloseButton)
         {
-            if (!Dialogs.ContainsKey(identifier))
+            if (Dialogs.TryGetValue(identifier, out DialogBox dialog))
             {
-                return;
+                dialog.Dispatcher.VerifyAccess();
+                dialog.DialogContent = content;
+                dialog.Title = string.IsNullOrEmpty(title) ? dialog.Title : title;
+                dialog.IsShowCloseButton = isShowCloseButton;
+                dialog.BeforeOpenHandler = openHandler;
+                dialog.AfterCloseHandler = closeHandle;
+                dialog.IsShow = true;
             }
-
-            DialogBox dialog = Dialogs[identifier];
-            dialog.Dispatcher.VerifyAccess();
-            dialog.DialogContent = content;
-            dialog.Title = string.IsNullOrEmpty(title) ? dialog.Title : title;
-            dialog.IsShowCloseButton = isShowCloseButton;
-            dialog.BeforeOpenHandler = openHandler;
-            dialog.AfterCloseHandler = closeHandle;
-            dialog.IsShow = true;
         }
 
         /// <summary>
@@ -60,9 +57,8 @@ namespace Rubyer
         /// <param name="identifier">DialogBox 标识</param>
         /// <param name="content">内容</param>
         public static void Show(string identifier, object content)
-        {
-            Show(identifier, content, "", null, null, true);
-        }
+            => Show(identifier, content, "", null, null, true);
+
         /// <summary>
         /// 显示指定对话框
         /// </summary>
@@ -70,9 +66,7 @@ namespace Rubyer
         /// <param name="content">内容</param>
         /// <param name="title">标题</param>
         public static void Show(string identifier, object content, string title)
-        {
-            Show(identifier, content, title, null, null, true);
-        }
+            => Show(identifier, content, title, null, null, true);
 
         /// <summary>
         /// 显示指定对话框
@@ -82,9 +76,8 @@ namespace Rubyer
         /// <param name="title">标题</param>
         /// <param name="isShowCloseButton">是否显示默认关闭按钮</param>
         public static void Show(string identifier, object content, string title, bool isShowCloseButton)
-        {
-            Show(identifier, content, title, null, null, isShowCloseButton);
-        }
+           => Show(identifier, content, title, null, null, isShowCloseButton);
+
 
         /// <summary>
         /// 显示指定对话框
@@ -93,9 +86,7 @@ namespace Rubyer
         /// <param name="content">内容</param>
         /// <param name="closeHandle">关闭后处理程序</param>
         public static void Show(string identifier, object content, Action<DialogBox, object> closeHandle)
-        {
-            Show(identifier, content, string.Empty, null, closeHandle, true);
-        }
+          => Show(identifier, content, string.Empty, null, closeHandle, true);
 
         /// <summary>
         /// 显示指定对话框
@@ -105,9 +96,7 @@ namespace Rubyer
         /// <param name="openHandler">打开前处理程序</param>
         /// <param name="closeHandle">关闭后处理程序</param>
         public static void Show(string identifier, object content, Action<DialogBox> openHandler, Action<DialogBox, object> closeHandle)
-        {
-            Show(identifier, content, string.Empty, openHandler, closeHandle, true);
-        }
+            => Show(identifier, content, string.Empty, openHandler, closeHandle, true);
 
         /// <summary>
         /// 显示指定对话框
@@ -117,9 +106,7 @@ namespace Rubyer
         /// <param name="closeHandle">关闭后处理程序</param>
         /// <param name="title">标题</param>
         public static void Show(string identifier, object content, string title, Action<DialogBox, object> closeHandle)
-        {
-            Show(identifier, content, title, null, closeHandle, true);
-        }
+            => Show(identifier, content, title, null, closeHandle, true);
 
         /// <summary>
         /// 显示指定对话框
@@ -130,9 +117,7 @@ namespace Rubyer
         /// <param name="closeHandle">关闭后处理程序</param>
         /// <param name="title">标题</param>
         public static void Show(string identifier, object content, string title, Action<DialogBox> openHandler, Action<DialogBox, object> closeHandle)
-        {
-            Show(identifier, content, title, openHandler, closeHandle, true);
-        }
+            => Show(identifier, content, title, openHandler, closeHandle, true);
 
         /// <summary>
         /// 关闭对话框
@@ -141,12 +126,10 @@ namespace Rubyer
         /// <param name="parameter">参数</param>
         public static void Close(string identifier, object parameter = null)
         {
-            if (!Dialogs.TryGetValue(identifier, out DialogBox dialogBox))
+            if (Dialogs.TryGetValue(identifier, out DialogBox dialogBox))
             {
-                return;
+                DialogBox.CloseDialogCommand.Execute(parameter, dialogBox);
             }
-
-            DialogBox.CloseDialogCommand.Execute(parameter, dialogBox);
         }
     }
 }
