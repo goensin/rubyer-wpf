d19e99c203b87a603fbf877951cc147b4a192693
wuyanxin <635831101@qq.com>
2022-06-02 15:32:24 +0800

    添加 ValidationModel 验证模型

---
+++ Rubyer/Helpers/TextBlockHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Rubyer
+{
+    public class TextBlockHelper
+    {
+        /// <summary>
+        /// 当被 Trim 时显示 Tool Tip
+        /// </summary>
+        public static readonly DependencyProperty IsTrimmedShowToolTipProperty =
+            DependencyProperty.RegisterAttached("IsTrimmedShowToolTip", typeof(bool), typeof(TextBoxHelper), new PropertyMetadata(false, OnIsTrimmedShowToolTipChanged));
+
+        private static void OnIsTrimmedShowToolTipChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBlock textBlock)
+            {
+                if (GetIsTrimmedShowToolTip(textBlock))
+                {
+                    GetIsTrimming(textBlock);
+                }
+            }
+        }
+
+        private static void TextBlock_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+           
+        }
+
+        private static void TextBlock_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var textBlock = (TextBlock)sender;
+        }
+
+        public static bool GetIsTrimming(TextBlock textBlock)
+        {
+
+            if (textBlock.TextTrimming == TextTrimming.None)
+            {
+                return false;
+            }
+
+
+            if (textBlock.TextWrapping == TextWrapping.NoWrap)  //比较长度
+            {
+                Size size = new Size(double.MaxValue, textBlock.RenderSize.Height);
+                textBlock.Measure(size);
+
+                if (textBlock.DesiredSize.Width > textBlock.RenderSize.Width)
+                {
+                    textBlock.Measure(textBlock.RenderSize);
+                    return true;
+                }
+
+            }
+            else  //比较高度
+            {
+                Size size = new Size(textBlock.RenderSize.Width,double.MaxValue);
+                textBlock.Measure(size);
+                if (textBlock.DesiredSize.Height > textBlock.RenderSize.Height)
+                {
+                    textBlock.Measure(textBlock.RenderSize);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool GetIsTrimmedShowToolTip(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsTrimmedShowToolTipProperty);
+        }
+
+        public static void SetIsTrimmedShowToolTip(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsTrimmedShowToolTipProperty, value);
+        }
+    }
+}
---
+++ Rubyer/Models/ValidationModel.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Rubyer.Models
+{
+    /// <summary>
+    /// 错误验证模型
+    /// </summary>
+    public class ValidationModel : INotifyDataErrorInfo, INotifyPropertyChanged
+    {
+        public ValidationModel()
+        {
+            Errors = new Dictionary<string, List<string>>();
+            PropertyChanged += (sender, e) => RefreshErrors(e.PropertyName);
+        }
+
+        /// <summary>
+        /// 所有错误
+        /// </summary>
+        protected Dictionary<string, List<string>> Errors { get; set; }
+
+        /// <summary>
+        /// 是否有错误
+        /// </summary>
+        public bool HasErrors => Errors.Count > 0;
+
+        /// <summary>
+        /// 错误改变事件
+        /// </summary>
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        /// <summary>
+        /// 属性改变事件
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 获取错误
+        /// </summary>
+        /// <param name="propertyName">属性名称</param>
+        /// <returns>错误集合</returns>
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                return Errors.Count > 0 ? Errors.First().Value : new List<string>();
+            }
+            else
+            {
+                return Errors.ContainsKey(propertyName) ? Errors[propertyName] : new List<string>();
+            }
+        }
+
+        /// <summary>
+        /// 设置属性错误
+        /// </summary>
+        /// <param name="propertyErrors">错误集合</param>
+        /// <param name="propertyName">属性名称</param>
+        protected void SetErrors(List<string> propertyErrors, [CallerMemberName] string propertyName = null)
+        {
+            Errors.Remove(propertyName);
+            Errors.Add(propertyName, propertyErrors);
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// 设置属性错误
+        /// </summary>
+        /// <param name="propertyError">错误</param>
+        /// <param name="propertyName">属性名称</param>
+        protected void SetErrors(string propertyError, [CallerMemberName] string propertyName = null) => SetErrors(new List<string> { propertyError }, propertyName);
+
+        /// <summary>
+        /// 清除属性的错误
+        /// </summary>
+        /// <param name="propertyName">属性名称</param>
+        protected void ClearErrors([CallerMemberName] string propertyName = null)
+        {
+            Errors.Remove(propertyName);
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// 清除所有错误
+        /// </summary>
+        protected void ClearAllErrors()
+        {
+            foreach (var property in GetType().GetProperties())
+            {
+                Errors.Remove(property.Name);
+                ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(property.Name));
+            }
+        }
+
+        /// <summary>
+        /// 刷新所有错误验证
+        /// </summary>
+        public void RaiseErrorValidation()
+        {
+            var properties = GetType().GetProperties().Where(x => x.Name != nameof(HasErrors));
+            foreach (var property in properties)
+            {
+                RefreshErrors(property);
+            }
+        }
+
+        /// <summary>
+        /// 判断属性是否有错误
+        /// </summary>
+        /// <param name="propertyName">属性名称</param>
+        /// <returns>是否有错误</returns>
+        public bool HasError([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName != null)
+            {
+                if (GetErrors(propertyName) is List<string> errors)
+                {
+                    return errors.Any();
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 设置属性值，并刷新属性错误
+        /// </summary>
+        /// <typeparam name="T">值类型</typeparam>
+        /// <param name="storage">字段引用</param>
+        /// <param name="value">值</param>
+        /// <param name="propertyName">属性名称</param>
+        public void SetValidationProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            RefreshErrors(propertyName);
+        }
+
+        private void RefreshErrors(string propertyName)
+        {
+            ClearErrors(propertyName);
+            if (propertyName != null)
+            {
+                var property = GetType().GetProperty(propertyName);
+                if (property != null)
+                {
+                    RefreshErrors(property);
+                }
+            }
+        }
+
+        private void RefreshErrors(PropertyInfo property)
+        {
+            var errors = new List<string>();
+            var attributes = (ValidationAttribute[])property.GetCustomAttributes(typeof(ValidationAttribute), false);
+            foreach (var attribute in attributes)
+            {
+                if (!attribute.IsValid(property.GetValue(this)))
+                {
+                    var displayAttribute = (DisplayAttribute)property.GetCustomAttributes(typeof(DisplayAttribute), false).FirstOrDefault();
+                    string name = displayAttribute == null ? property.Name : displayAttribute.Name;
+                    errors.Add(attribute.FormatErrorMessage(name ?? string.Empty));
+                }
+            }
+
+            if (errors.Count > 0)
+            {
+                SetErrors(errors, property.Name);
+            }
+        }
+    }
+}
