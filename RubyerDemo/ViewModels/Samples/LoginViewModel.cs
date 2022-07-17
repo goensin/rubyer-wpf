9631b0cf948b73fef323fbb38d44394b25e796f5
吴燕新 <635831101@qq.com>
2022-07-17 22:42:34 +0800

    login view 文件

---
+++ RubyerDemo/ViewModels/Samples/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubyerDemo.ViewModels.Samples
+{
+    /// <summary>
+    ///  登录页 view model
+    /// </summary>
+    public class LoginViewModel : ViewModelBase
+    {
+    }
+}
