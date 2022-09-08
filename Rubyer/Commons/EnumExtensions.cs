9420a99e8c9118f28623ba75a3fc96c332d3df97
wuyanxin <635831101@qq.com>
2022-09-08 16:22:35 +0800

    添加 EnumGetDescription 转换器

---
+++ Rubyer/Commons/EnumExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Rubyer.Commons
+{
+    /// <summary>
+    /// enum 扩展方法
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// 获取描述
+        /// </summary>
+        /// <param name="value">枚举值</param>
+        /// <returns>描述</returns>
+        public static string GetDescription(this Enum value)
+        {
+            if (value != null)
+            {
+                FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+                if (fieldInfo != null)
+                {
+                    var attributes = fieldInfo.GetCustomAttributes<DescriptionAttribute>(inherit: false);
+                    return ((attributes.Count() > 0) && (!string.IsNullOrEmpty(attributes.First().Description)))
+                        ? attributes.First().Description
+                        : value.ToString();
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ Rubyer/Converters/EnumGetDescriptionConverter.cs
@@ -0,0 +1,30 @@
+﻿using Rubyer.Commons;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Rubyer.Converters
+{
+    /// <summary>
+    /// 枚举获取说明
+    /// </summary>
+    public class EnumGetDescriptionConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var enumValue = ValidateArgument.NotNullOrEmptyCast<Enum>(value, "value");
+            return enumValue.GetDescription();
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
