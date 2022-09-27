478301d17b5cc8bb2515f4e059f267c9481fc8e5
wuyanxin <635831101@qq.com>
2022-09-27 17:47:01 +0800

    MenuBase 添加 rubyer:MenuHelper.IconWidth 附加属性设置 icon 宽度

---
+++ Rubyer/Helpers/MenuHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace Rubyer
+{
+    /// <summary>
+    /// Menu 帮助类
+    /// </summary>
+    public class MenuHelper
+    {
+        /// <summary>
+        /// 图标宽度
+        /// </summary>
+        public static readonly DependencyProperty IconWidthProperty = DependencyProperty.RegisterAttached(
+            "IconWidth", typeof(double), typeof(MenuHelper), new PropertyMetadata(default(double)));
+
+        /// <summary>
+        /// Sets the IconWidth.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <param name="value">If true, value.</param>
+        public static void SetIconWidth(DependencyObject element, double value)
+        {
+            element.SetValue(IconWidthProperty, value);
+        }
+
+        /// <summary>
+        /// Gets the IconWidth.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <returns>A bool.</returns>
+        public static double GetShowShadow(DependencyObject element)
+        {
+            return (double)element.GetValue(IconWidthProperty);
+        }
+    }
+}
