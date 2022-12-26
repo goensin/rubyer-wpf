36d5b7aaa5da47e24553948720cfd4602e1921b1
wuyanxin <635831101@qq.com>
2022-12-26 15:05:46 +0800

    自定义 TreeView 展开按钮图标

---
+++ Rubyer/Helpers/TreeViewHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace Rubyer
+{
+    /// <summary>
+    /// TreeView 帮助类
+    /// </summary>
+    public class TreeViewHelper
+    {
+        /// <summary>
+        /// 展开图标类型
+        /// </summary>
+        public static readonly DependencyProperty ExpandIconTypeProperty = DependencyProperty.RegisterAttached(
+            "ExpandIconType", typeof(IconType), typeof(TreeViewHelper), new PropertyMetadata(IconType.ArrowRightSFill));
+
+        public static void SetExpandIconType(DependencyObject element, IconType value)
+        {
+            element.SetValue(ExpandIconTypeProperty, value);
+        }
+
+        public static IconType GetExpandIconType(DependencyObject element)
+        {
+            return (IconType)element.GetValue(ExpandIconTypeProperty);
+        }
+    }
+}
