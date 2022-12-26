99e2e6d73a0fae1434faa8d5d5f0a417b9281614
wuyanxin <635831101@qq.com>
2022-12-26 15:22:36 +0800

    Expander 添加 ExpanderHelper.ExpandIconType 修改展开按钮图标

---
+++ Rubyer/Helpers/ExpanderHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace Rubyer
+{
+    public class ExpanderHelper
+    {
+        /// <summary>
+        /// 展开图标类型
+        /// </summary>
+        public static readonly DependencyProperty ExpandIconTypeProperty = DependencyProperty.RegisterAttached(
+            "ExpandIconType", typeof(IconType), typeof(ExpanderHelper), new PropertyMetadata(IconType.ArrowDownSLine));
+
+        public static void SetExpandIconType(DependencyObject element, IconType value)
+        {
+            element.SetValue(ExpandIconTypeProperty, value);
+        }
+
+        public static IconType GetExpandIconType(DependencyObject element)
+        {
+            return (IconType)element.GetValue(ExpandIconTypeProperty);
+        }
+    }
+}
