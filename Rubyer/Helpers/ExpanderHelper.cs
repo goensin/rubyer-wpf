3caf36df4912fb3e604ed504e20df6760dc5878b
wuyanxin <635831101@qq.com>
2022-12-26 15:34:52 +0800

    Expander 添加 ExpanderHelper.ExpandIconDock 控制展开图标 位置

--- Rubyer/Helpers/ExpanderHelper.cs
+++ Rubyer/Helpers/ExpanderHelper.cs
@@ -1,7 +1,11 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Rubyer
 {
+    /// <summary>
+    /// Expander 帮助类
+    /// </summary>
     public class ExpanderHelper
     {
         /// <summary>
@@ -18,6 +22,22 @@ namespace Rubyer
         public static IconType GetExpandIconType(DependencyObject element)
         {
             return (IconType)element.GetValue(ExpandIconTypeProperty);
+        }
+
+        /// <summary>
+        /// 展开图标类型
+        /// </summary>
+        public static readonly DependencyProperty ExpandIconDockProperty = DependencyProperty.RegisterAttached(
+            "ExpandIconDock", typeof(Dock), typeof(ExpanderHelper), new PropertyMetadata(Dock.Right));
+
+        public static void SetExpandIconDock(DependencyObject element, Dock value)
+        {
+            element.SetValue(ExpandIconDockProperty, value);
+        }
+
+        public static Dock GetExpandIconDock(DependencyObject element)
+        {
+            return (Dock)element.GetValue(ExpandIconDockProperty);
         }
     }
 }
