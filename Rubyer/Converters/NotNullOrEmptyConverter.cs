47ade15d05faecfc7ef100fdf90d623846a0e92d
wuyanxin <635831101@qq.com>
2023-04-17 14:43:24 +0800

    Add： NotNullOrEmptyConverter

---
+++ Rubyer/Converters/NotNullOrEmptyConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Rubyer.Converters
+{
+    /// <summary>
+    /// string 不为 null 或者 empty
+    /// </summary>
+    public class NotNullOrEmptyConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && !string.IsNullOrEmpty(value.ToString());
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
