2d9003748f7beb991fd138b61efe6a913839c7e8
wuyanxin <635831101@qq.com>
2023-06-25 11:12:59 +0800

    Fix: PasswordBox: ErrorTemplate 找不到报错

--- RubyerDemo/ViewModels/PasswordBoxViewModel.cs
+++ RubyerDemo/ViewModels/PasswordBoxViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,12 @@ namespace RubyerDemo.ViewModels
     /// <summary>
     /// 密码框
     /// </summary>
-    public partial class PasswordBoxViewModel : ObservableObject
+    public partial class PasswordBoxViewModel : ObservableValidator
     {
         [ObservableProperty]
+        [NotifyDataErrorInfo]
+        [MaxLength(10, ErrorMessage = "密码长度不能超过 {1}")]
+        [Required]
         private string password = "123456";
     }
 }
