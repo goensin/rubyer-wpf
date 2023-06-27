d84327602cf81c7d6d08dd6607dc1fe65dbd2a52
wuyanxin <635831101@qq.com>
2023-06-27 17:52:39 +0800

    Add: NullToVisibilityConverter

---
+++ Rubyer/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Rubyer.Converters
+{
+    /// <summary>
+    /// null -> Visiblity
+    /// </summary>
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// null 对应 Visibility
+        /// </summary>
+        public Visibility NullVisibility { get; set; }
+
+        /// <summary>
+        /// 非 null 对应 Visibility    
+        /// </summary>
+        public Visibility NotNullVisibility { get; set; }
+
+        public NullToVisibilityConverter()
+        {
+            NullVisibility = Visibility.Collapsed;
+            NotNullVisibility = Visibility.Visible;
+        }
+
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is null ? NullVisibility : NotNullVisibility;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
