f0f047c544148562f5a4cd37a9ec4c02648d54b8
wuyanxin <635831101@qq.com>
2023-07-17 11:04:13 +0800

    添加 CloneConverter, 将 ToolTip 显示的内容克隆，防止 ToolTip 显示时将内容带到另一个 VisualTree

---
+++ Rubyer/Converters/CloneConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Rubyer.Converters
+{
+    /// <summary>
+    /// 克隆内容
+    /// </summary>
+    public class CloneConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return value;
+            }
+
+            string xaml = XamlWriter.Save(value);
+            var cloneValue = XamlReader.Parse(xaml);
+            return cloneValue;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
