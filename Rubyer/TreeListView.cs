43b994a80a2071dfdfd130ea34ef6223c4eca21a
wuyanxin <635831101@qq.com>
2023-08-04 17:21:36 +0800

    暂存

---
+++ Rubyer/TreeListView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Rubyer
+{
+    /// <summary>
+    /// 树形列表
+    /// </summary>
+    [StyleTypedProperty(Property = "ItemContainerStyle", StyleTargetType = typeof(TreeListViewItem))]
+    public class TreeListView : ListView
+    {
+        static TreeListView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TreeListView), new FrameworkPropertyMetadata(typeof(TreeListView)));
+        }
+
+        /// <inheritdoc/>
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is TreeListViewItem;
+        }
+
+        /// <inheritdoc/>
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new TreeListViewItem();
+        }
+    }
+}
---
+++ Rubyer/TreeListViewItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Rubyer
+{
+    /// <summary>
+    /// 树形列表项
+    /// </summary>
+    public class TreeListViewItem : ListViewItem
+    {
+        static TreeListViewItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TreeListViewItem), new FrameworkPropertyMetadata(typeof(TreeListViewItem)));
+        }
+    }
+}
